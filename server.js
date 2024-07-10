77b3cf4b4a06eb88304ee3b6d2f0eabda982622e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-07-10 11:55:31 +0530

    Create mongodb database and connect application in database success, add cookieparse middleware in server js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/database.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie parser
+app.use(cookieParser());
+
+// database connection
+connectDB();
+
 app.use('/api/users', userRoutes);
 
 app.use('/', (req, res) => {
